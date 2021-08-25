4dda48461c61ff70349b5db7ab1071f5c20ca3e3
Francesco Innocenti <infr.management@gmail.com>
2021-08-25 15:38:45 +0200

    refactor and move encoding script to work with both models

--- perform_encoding.py
+++ perform_encoding.py
@@ -1,10 +1,10 @@
 import numpy as np
-from utils import load_fmri, load_activations, vectorized_correlation
+from utils import load_fmri, vectorized_correlation
 from sklearn.linear_model import LinearRegression
 from sklearn.multioutput import MultiOutputRegressor
 
 
-def perform_encoding(pca_dir, fmri_dir, layer, sub, ROI):
+def perform_encoding(train_features, fmri_dir, sub, ROI):
     """This function linearly regresses pca-reduced features/activations of a
     given layer of a neural network to the fMRI activity of a given brain region
     (ROI) in a given subject. The fitted model is used to predict part of the
@@ -31,17 +31,14 @@ def perform_encoding(pca_dir, fmri_dir, layer, sub, ROI):
 
     """
 
-    # load train activations
-    train_activations = load_activations(pca_dir, layer)
-
     # load fMRI data for a given subject and ROI
     fmri_train_all = load_fmri(fmri_dir, sub, ROI)
     # get number of voxels for given ROI
     num_voxels = fmri_train_all.shape[1]
 
     # create training and validation sets
-    val_activations = train_activations[900:,:]
-    train_activations = train_activations[:900,:]
+    val_features = train_features[900:,:]
+    train_features = train_features[:900,:]
     fmri_train = fmri_train_all[:900,:]
     fmri_val = fmri_train_all[900:,:]
 
@@ -50,15 +47,12 @@ def perform_encoding(pca_dir, fmri_dir, layer, sub, ROI):
 
     # perform multiple multivariate regression
     reg = MultiOutputRegressor(LinearRegression())
-    reg.fit(train_activations, fmri_train)
-    pred_fmri = reg.predict(val_activations)
+    reg.fit(train_features, fmri_train)
+    pred_fmri = reg.predict(val_features)
 
     # correlation between validation predictions and ground truth
     corr = vectorized_correlation(fmri_val, pred_fmri)
-    print("----------------------------------------------------------------------------")
     # get mean correlation
     voxelwise_corr = round(corr.mean(), 6)
-    print("Mean correlation for ROI : " ,ROI ," in " ,sub ," using " ,layer
-          ," is :", voxelwise_corr)
 
     return voxelwise_corr
